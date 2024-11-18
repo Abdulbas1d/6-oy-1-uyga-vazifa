77a2e483cd3554b5102cdd558ece3d30d8019087
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-11-19 04:43:43 +0500

    Header finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
+import Logo from './assets/images/logo.svg'
 import Header from './assets/images/header.png'
+import Solid from './assets/images/solid.svg'
 import HeaderBottom from './assets/images/header-bottom.svg'
 
 function App() {
@@ -7,11 +9,22 @@ function App() {
     <div className="container">
       <div className="header">
         <div className="logo">
-          <h1>myteam</h1>
-          <a href="#">home</a>
+          <img src={Logo} alt="" />
+          <a className='first' href="#">home</a>
           <a href="#">about</a>
           <button className='btn'>contact us</button>
         </div>
+        
+        <div className="hero">
+          <img src={Header} alt="" />
+          <h1>Find the best <span>talent</span></h1>
+          <div className="text">
+            <img src={Solid} alt="" />
+            <p>Finding the right people and building high performing teams can be hard. Most companies aren’t tapping into the abundance of global talent. We’re about to change that.</p>
+          </div>
+        </div>
+
+        <img className='header-bottom-picture' src={HeaderBottom} alt="" />
       </div>
     </div>
   )
