67796404181cc68905bad9a4f847f6d23e4e1bc0
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-11-19 03:51:24 +0500

    Many code write

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 import './App.css'
+import Header from './assets/images/header.png'
+import HeaderBottom from './assets/images/header-bottom.svg'
 
-function App() {}
+function App() {
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="logo">
+          <h1>myteam</h1>
+          <a href="#">home</a>
+          <a href="#">about</a>
+          <button className='btn'>contact us</button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default App
